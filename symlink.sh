e8df834d634702c260c627bf86fa82b2c92f43b4
Mitja Felicijan <mitja.felicijan@gmail.com>
2024-10-14 19:19:28 +0200

    Move to newsraft from newsboat

--- symlink.sh
+++ symlink.sh
@@ -1,7 +1,5 @@
 ln -s ~/.dotfiles/shenanigans.sh ~/.shenanigans.sh
 ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
 ln -s ~/.dotfiles/vimrc ~/.vimrc
-ln -s ~/.dotfiles/newsboat ~/.newsboat/config
-ln -s ~/.dotfiles/urls ~/.newsboat/urls
 ln -s ~/.dotfiles/i3config ~/.config/i3/config
 

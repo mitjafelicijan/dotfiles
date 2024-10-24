6ea9f84e6af1ff021cc73a782b0c79b1f5fbdb35
Mitja Felicijan <mitja.felicijan@gmail.com>
2024-10-25 00:31:24 +0200

    Update

--- symlink.sh
+++ symlink.sh
@@ -2,5 +2,6 @@ ln -s ~/.dotfiles/shenanigans.sh ~/.shenanigans.sh
 ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
 ln -s ~/.dotfiles/vimrc ~/.vimrc
 ln -s ~/.dotfiles/i3config ~/.config/i3/config
+ln -s ~/.dotfiles/i3status  ~/.config/i3status/config
 ln -s ~/.dotfiles/rssfeeds  ~/.newsboat/urls
 

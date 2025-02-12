ab26783d64b186d414bcc1ee085e59f16b71d528
Mitja Felicijan <mitja.felicijan@gmail.com>
2025-02-12 07:15:36 +0100

    Update

--- .shenanigans.sh
+++ .shenanigans.sh
@@ -90,7 +90,6 @@ backup() {
 		--exclude '.godot/' \
 		--exclude '.zig-cache/' \
 		--exclude 'node_modules/' \
-		--exclude 'digg-v5/' \
 		--delete \
 		$VHOME/ /media/Void/Backup/$ME/
 
@@ -100,28 +99,5 @@ backup() {
 
 	# Return back to original directory
 	cd $CWD
-}
-
-# Toggles between pulseaudio sinks in round-robin.
-togglesink() {
-	sinks=($(pactl list short sinks | awk '{print $2}'))
-	current_sink=$(pactl get-default-sink)
-	current_index=-1
-
-	for i in "${!sinks[@]}"; do
-		if [[ "${sinks[$i]}" == "$current_sink" ]]; then
-			current_index=$i
-			break
-		fi
-	done
-
-	if [[ $current_index -eq -1 ]]; then
-		next_index=0
-	else
-		next_index=$(( (current_index + 1) % ${#sinks[@]} ))
-	fi
-
-	pactl set-default-sink "${sinks[$next_index]}"
-	notify-send "Switched to sink: ${sinks[$next_index]}"
 }
 

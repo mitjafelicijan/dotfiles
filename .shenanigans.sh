a691b3d7154450305eece81677a37986809d359b
Mitja Felicijan <mitja.felicijan@gmail.com>
2025-07-16 17:55:43 +0200

    Update

--- .shenanigans.sh
+++ .shenanigans.sh
@@ -2,7 +2,8 @@
 #   void-repo-nonfree void-repo-multilib void-repo-multilib-nonfree
 #   file-roller xfce4-screenshooter xfce4-plugins xsetroot thunar-archive-plugin
 #   clang clang-tools-extra vim stow git curl tmux hstr tree make cmake xclipi jq
-#   entr ack lazygit newsboat htop mc mpv cifs-utils zip rsync python3-pipx jq conky
+#   entr ack lazygit newsboat htop mc mpv cifs-utils zip rsync python3-pipx jq
+#   lm_sensors conky
 # Additonal stuff:
 #   pipx install pyright
 #   go install golang.org/x/tools/gopls@latest

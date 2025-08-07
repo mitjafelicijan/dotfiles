c72e17e0f70db67bb234ca5131ba15d723b2d517
Mitja Felicijan <mitja.felicijan@gmail.com>
2025-08-07 09:11:52 +0200

    Update

--- .shenanigans.sh
+++ .shenanigans.sh
@@ -4,7 +4,7 @@
 #   lm_sensors conky maim xlockmore rofi picom cwm xclip xsetroot st
 #   xss-lock wmctrl zip mc htop entr cifs-utils rsync jq ripgrep gdb
 #   clang clang-tools-extra vim git curl tmux hstr tree make cmake
-#   nvtop ctags stow newsboat mpv rsync python3-pipx lazygit tcc
+#   nvtop ctags stow newsboat mpv rsync python3-pipx lazygit tcc xdotool
 # Additonal stuff:
 #   pipx install pyright
 #   go install golang.org/x/tools/gopls@latest
